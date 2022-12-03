534d236f0b15b3e215252cb05eec62290df76252
Xingjian Xuanyuan <xingjianxuanyuan@lochlomond.dhcp.wustl.edu>
2022-12-03 16:27:23 -0600

    Update ruby

--- src/main/ruby/powers_of_two_range/powers_of_two_range.rb
+++ src/main/ruby/powers_of_two_range/powers_of_two_range.rb
@@ -1,23 +1,79 @@
-# __STUDENT_NAME__
+# Xingjian Xuanyuan
 # Dennis Cosgrove
 
 class PowersOfTwoRange
   
   def initialize(maximum, inclusive: false)
-    
-    raise "not_yet_implemented"
-
+    @maximum = maximum
+    @inclusive = inclusive
   end
 
   def each
-    
-    raise "not_yet_implemented"
-
+    if @inclusive
+      if block_given?
+        if @maximum < 1
+          yield []
+        else
+          tmp = 1
+          while tmp <= @maximum do
+            yield tmp
+            tmp *= 2
+          end
+        end
+      else
+        to_enum(__method__)
+      end
+    else
+      if block_given?
+        if @maximum < 1
+          yield []
+        else
+          tmp = 1
+          while tmp < @maximum do
+            yield tmp
+            tmp *= 2
+          end
+        end
+      else
+        to_enum(__method__)
+      end
+    end
   end
 
   def each_with_index
-    
-    raise "not_yet_implemented"
-
+    if @inclusive
+      if block_given?
+        if @maximum < 1
+          yield []
+        else
+          tmp = 1
+          pow = 0
+          while tmp <= @maximum do
+            yield tmp, pow
+            tmp *= 2
+            pow += 1
+          end
+        end
+      else
+        to_enum(__method__)
+      end
+    else
+      if block_given?
+        if @maximum < 1
+          yield []
+        else
+          tmp = 1
+          pow = 0
+          while tmp < @maximum do
+            yield tmp, pow
+            tmp *= 2
+            pow += 1
+          end
+        end
+      else
+        to_enum(__method__)
+      end
+    end  
   end
+
 end
